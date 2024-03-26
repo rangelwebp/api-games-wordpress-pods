ff7aa473a1c65b86f8505c9288875c7264aafbee
Rangel Xavier <rangel.webp@gmail.com>
2024-03-26 14:04:50 -0300

    Melhorias
    Melhorias no retorno como array

--- functions.php
+++ functions.php
@@ -11,81 +11,82 @@ add_action( 'rest_api_init', function () {
     ) );
 } );
 
-  function get_sliders_api(){
-    $sliders = array();
-    $params = array(
-        "limit" => -1,
-        "post_status" => "publish",
-        "where" => "slider.meta_value =" . 1
-    );
+function get_sliders_api() {
+  $sliders = array();
+  $params = array(
+      "limit" => -1,
+      "post_status" => "publish",
+      "where" => "slider.meta_value = 1"
+  );
 
-    $mypod = pods( 'games', $params );
-    $sliders['total'] = $mypod->total();
+  $mypod = pods('games', $params);
+  // Aqui, você inicializa 'sliders' como um array vazio para garantir que ele seja interpretado como um array
+  $sliders['sliders'] = array();
+  $sliders['total'] = $mypod->total();
 
-    $chaveId = 0;
-    while ( $mypod->fetch() ) {
-        $chaveId++;
-        $slider = array(
-            'id' => $mypod->field('ID'),
-            'nome' => $mypod->field('post_title'),
-            'slug' => $mypod->field('slug'),
-            'categoria_principal' => $mypod->field('categoria_principal'),
-            'traducao' => $mypod->field('traducao'),
-            'nota' => $mypod->field('nota'),
-            'classificacao' => $mypod->field('classificacao'),
-            'indicacao' => $mypod->field('indicacao'),
-            'imagem' => $mypod->field('imagens_sliders')[0]['guid'],
-        );
-        $sliders['sliders'][$chaveId] = $slider;
-    }
-    
-
-    return rest_ensure_response ( $sliders );    
+  while ($mypod->fetch()) {
+      $slider = array(
+          'id' => $mypod->field('id'),
+          'nome' => $mypod->field('post_title'),
+          'slug' => $mypod->field('slug'),
+          'descricao' => wp_strip_all_tags ($mypod->field('post_content')),
+          'categoria_principal' => $mypod->field('categoria_principal'),
+          'traducao' => $mypod->field('traducao'),
+          'nota' => $mypod->field('nota'),
+          'classificacao' => $mypod->field('classificacao'),
+          'indicacao' => $mypod->field('indicacao'),
+          // Certifique-se de verificar se 'imagens_sliders' existe e tem itens antes de acessar [0]
+          'imagem' => isset($mypod->field('imagens_sliders')[0]) ? $mypod->field('imagens_sliders')[0]['guid'] : null,
+      );
+      // Adicionando cada slider ao array de 'sliders', sem usar chave associativa
+      $sliders['sliders'][] = $slider;
   }
+
+  return rest_ensure_response($sliders);
+}
+
 
 // Trine Series
 // **********
 add_action( 'rest_api_init', function () {
-    register_rest_route( 'games/v1', '/trines', array(
+    register_rest_route( 'games/v1', '/seriestrines', array(
       'methods' => 'GET',
       'callback' => 'get_trines_api',
     ) );
 } );
 
   function get_trines_api(){
-    $trines = array();
+    $result = array();
     
     $params = array(
         "limit" => -1,
         "post_status" => "publish",
-        "where" => "trine.meta_value =" . 1 . " AND complemento.meta_value = " . 0
-        // "where" => "trine.meta_value =" . 1 . "AND complemento.meta_value = " . 0
+        "where" => "trine.meta_value = 1 AND complemento.meta_value = 0"
     );
 
     $mypod = pods( 'games', $params );
-    $trines['total'] = $mypod->total();
+    $result['t'] = array();
+    $result['total'] = $mypod->total();
 
-    $chaveId = 0;
     while ( $mypod->fetch() ) {
-        $chaveId++;
-        $trine = array(
-            'id' => $mypod->field('ID'),
+        $itemTrine = array(
+            'id' => $mypod->field('id'),
             'nome' => $mypod->field('post_title'),
             'thumb' => $mypod->field('thumbnail')['guid'],
             'slug' => $mypod->field('slug'),
             'nota' => $mypod->field('nota'),
         );
-        $trines['trines'][$chaveId] = $trine;
+        $result['t'][] = $itemTrine;
     }
 
-    return rest_ensure_response ( $trines );
+    return rest_ensure_response ( $result );
 
   }
 
 // Todos os jogos
 // **********
 add_action( 'rest_api_init', function () {
-    register_rest_route( 'games/v1', '/games', array(
+    register_rest_route( 'games/v1', '/all-games', array(
       'methods' => 'GET',
       'callback' => 'get_all_games_api',
     ) );
@@ -101,18 +102,17 @@ add_action( 'rest_api_init', function () {
 
     $mypod = pods( 'games', $params );
     $games['total'] = $mypod->total();
+    $games['games'] = array();
 
-    $chaveId = 0;
     while ( $mypod->fetch() ) {
-        $chaveId++;
-        $trine = array(
+        $item = array(
             'id' => $mypod->field('ID'),
             'nome' => $mypod->field('post_title'),
             'slug' => $mypod->field('slug'),
             'nota' => $mypod->field('nota'),
             'thumb' => $mypod->field('thumbnail')['guid'],
         );
-        $games['games'][$chaveId] = $trine;
+        $games['games'][] = $item;
     }
 
     return rest_ensure_response ( $games );
@@ -133,23 +133,23 @@ function get_anothers_api(){
   $params = array(
       "limit" => -1,
       "post_status" => "publish",
-      "where" => "trine.meta_value =" . 0
+      // "where" => "trine.meta_value =" . 0,
+      "where" => "trine.meta_value = 0 AND complemento.meta_value = 0 AND slider.meta_value = 0"
   );
 
   $mypod = pods( 'games', $params );
   $anothers['total'] = $mypod->total();
+  $anothers['games'] = array();
 
-  $chaveId = 0;
   while ( $mypod->fetch() ) {
-      $chaveId++;
-      $trine = array(
+      $item = array(
           'id' => $mypod->field('ID'),
           'nome' => $mypod->field('post_title'),
           'thumb' => $mypod->field('thumbnail')['guid'],
           'slug' => $mypod->field('slug'),
           'nota' => $mypod->field('nota'),
       );
-      $anothers['anothers'][$chaveId] = $trine;
+      $anothers['games'][] = $item;
   }
 
   return rest_ensure_response ( $anothers );
